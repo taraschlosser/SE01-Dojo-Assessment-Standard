1f240ead525479e068a61cda5f7de0d8e98e5b27
Tara Schlosser <tara.schlosser@code.berlin>
2024-11-27 15:03:16 +0100

    Exercise 1: Mastermind Game

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("test")
+def mastermind():
+  
+  tries = 12
+  code = "4431"
+
+  while tries > 0:
+    guess = input("Guess the code: ")
+    tries = tries-1
+
+    if guess == code: 
+      print ("You won the game")
+      break
+    
+    rightposition = 0
+    
+    for idx, number in enumerate(guess):
+      if number == code[idx]:
+        rightposition = rightposition +1
+    print (str(rightposition) + " right position(s) in code")
+
+  
+mastermind()
