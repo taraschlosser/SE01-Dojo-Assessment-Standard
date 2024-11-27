4bc6327fc2e9852be22b750767add6b711202c6e
Tara Schlosser <tara.schlosser@code.berlin>
2024-11-27 15:48:25 +0100

    Exercise 2

--- main.py
+++ main.py
@@ -12,11 +12,16 @@ def mastermind():
       break
     
     rightposition = 0
+    rightnumber = 0
     
     for idx, number in enumerate(guess):
       if number == code[idx]:
         rightposition = rightposition +1
+      if number in code:
+        rightnumber = rightnumber +1
+    rightnumber = rightnumber - rightposition
     print (str(rightposition) + " right position(s) in code")
+    print (str(rightnumber) + " right numbers in code")
 
   
 mastermind()
